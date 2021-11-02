f351257036da4fa38820a0db9c697966296b64f5
koddi9 <mors.1.course@gmail.com>
2021-11-02 05:22:42 +0300

    badTest1

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,14 +1,18 @@
 package web.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import web.model.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
+@RequestMapping("/main")
 public class HelloController {
 
 	@GetMapping(value = "/")
@@ -19,6 +23,20 @@ public class HelloController {
 		messages.add("5.2.0 version by sep'19 ");
 		model.addAttribute("messages", messages);
 		return "index";
+	}
+
+	@GetMapping(value = "/cars")
+	public String printCars(ModelMap model,@RequestParam(name="count",required = false) String count) {
+		List<Car> cars = new ArrayList<>();
+		Integer countInt=(Integer) model.getAttribute("count");
+		cars.add(new Car("BMW",5,"gray"));
+		cars.add(new Car("2",2,"black"));
+		cars.add(new Car("3",3,"white"));
+		cars.add(new Car("4",4,"green"));
+		cars.add(new Car("5",5,"red"));
+		cars=cars.stream().limit(Integer.parseInt(count)).collect(Collectors.toList());
+		model.addAttribute("cars", cars);
+		return "cars";
 	}
 	
 }
